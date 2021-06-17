43bde44fa11e61808b5c1a806653053e7a929039
george murphy <georgeolivermurphy@hotmail.com>
2021-06-17 14:27:09 +0100

    handle range

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -26,6 +26,10 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  console.log(this.value);
+}
+
 // Hook Up the event Listeners //
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -35,3 +39,5 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click',
  skip));
+ranges.forEach(range => range.addEventListener('change',
+  handleRangeUpdate))
